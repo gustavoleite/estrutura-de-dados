6b654b6014edc1ee2a8d3e8adc5f33eab7b8b57a
Gustavo Guterres Leite <gustavo.jog@hotmail.com>
2018-03-08 17:53:37 -0300

    Adicionado método de insercao

--- Arvore-Binaria/main.c
+++ Arvore-Binaria/main.c
@@ -1,30 +1,86 @@
+typedef struct NO* ArvBin;
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
-//define uma nova struct (primitivo de classe)
 struct NO {
-    int info;
+    int valor;
     struct NO *esq;
     struct NO *dir;
 };
 
-//defino um novo tipo chamado ArvBin que é do tipo NO
-typedef struct NO* ArvBin;
-
-//declaro um ponteiro global do tipo ArvBin
-ArvBin* raiz;
-
-//aloca espaco para uma nova variavel do tipo ArvBin
 ArvBin* cria_ArvBin() {
-    ArvBin* arvBin = (ArvBin*) malloc(sizeof (ArvBin));
-    if (arvBin != NULL) {
-        *arvBin = NULL;
+    ArvBin* raiz = (ArvBin*) malloc(sizeof (ArvBin));
+    if (raiz != NULL) {
+
+        *raiz = NULL;
     }
-    return arvBin;
-};
-
-
-int main(void) {
-    raiz = cria_ArvBin();
-
+    return raiz;
 }
+
+struct NO* criarFolha(int valor) {
+    struct NO* folha = (struct NO*) malloc(sizeof (struct NO));
+    if (folha == NULL) {
+        return 0;
+    }
+    folha->valor = valor;
+    folha->dir = NULL;
+    folha->esq = NULL;
+    return folha;
+}
+
+bool arvoreIsEmpty(ArvBin const *raiz);
+
+bool valorDuplicado(int valor, const struct NO *atual);
+
+void insere_ArvBin(ArvBin* raiz, int valor) {
+    if (raiz == NULL) {
+        return;
+    }
+    struct NO* novo = criarFolha(valor);
+
+    if (arvoreIsEmpty(raiz)) {
+        *raiz = novo;
+    } else {
+        struct NO* atual = *raiz;
+        struct NO* ant = NULL;
+        while (atual != NULL) {
+            ant = atual;
+            if (valorDuplicado(valor, atual)) {
+                free(novo);
+                return;
+            }
+            if (valor > atual->valor) {
+                atual = atual->dir;
+            } else {
+                atual = atual->esq;
+            }
+        }
+        if (valor > ant->valor) {
+            ant->dir = novo;
+        } else {
+            ant->esq = novo;
+        }
+    }
+}
+
+bool valorDuplicado(int valor, const struct NO *atual) { return valor == atual->valor; }
+
+bool arvoreIsEmpty(ArvBin const *raiz) { return *raiz == NULL; }
+
+
+int main(int argc, char** argv) {
+    int v;
+    ArvBin* raiz = cria_ArvBin();
+    do {
+        printf("Digite um valor: ");
+        scanf("%d", &v);
+        if (v > 0) {
+            insere_ArvBin(raiz, v);
+        }
+    } while (v != 0);
+
+    return (EXIT_SUCCESS);
+}
+
